919afb1ecec1c7cbe34dc4405a987998ab9c278f
Dict Xiong <me@beardic.cn>
2022-01-11 16:05:00 +0800

    version 0.1
    - add command `/adc` and according permission
    - add command `/adc reload` to reload rules
    - add command `/adc trigger <player> <advancement>` to revoke then grant
      advancement to player (mainly for test)
    - add four command placeholders `<adc:uuid>` `<adc:player_name>` `<adc:display_name>`
      `<adc:advancement_id>`, and they will be replaced accordingly while
      execution.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,15 @@
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements CommandExecutor {
     @Override
     public void onLoad() {
         saveDefaultConfig();
@@ -23,7 +27,7 @@ public class Main extends JavaPlugin {
 
         //绑定指令与执行器
         //注：此处绑定的指令必须要先在plugin.yml中注册
-        // Bukkit.getPluginCommand("demo").setExecutor(new Executor());
+        Bukkit.getPluginCommand("adc").setExecutor(this);
         //注册监听器
         Bukkit.getPluginManager().registerEvents(new Monitor(config), this);
         Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>§e enabled.");
@@ -31,7 +35,39 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        HandlerList.unregisterAll(this);
         Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>§e disabled.");
+    }
+
+    public void reload() {
+        onDisable();
+        reloadConfig();
+        onEnable();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String str, String[] args) {
+        if (sender.hasPermission("advancementcommand")) {
+            if (args != null && args.length >= 1 && Objects.equals(args[0], "reload")) {
+                sender.sendMessage("§f<§aAdvancementCommand§f>§e reloading...");
+                reload();
+            } else if (args != null && args.length >= 1 && Objects.equals(args[0], "trigger")) {
+                if (args.length < 3)
+                {
+                    sender.sendMessage("Usage: /adc trigger <player> <advancement>");
+                }
+                else
+                {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "advancement revoke " + args[1] + " only " + args[2]);
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "advancement grant " + args[1] + " only " + args[2]);
+                }
+            } else {
+                sender.sendMessage("Usage:\n  /adc reload - reload rules\n  /adc trigger <player> <advancement> - revoke and grant advancement to player");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "You are not permitted to use this command.");
+        }
+        return true;
     }
 
 }
--- src/Main/Monitor.java
+++ src/Main/Monitor.java
@@ -1,5 +1,5 @@
-import org.apache.commons.lang.ObjectUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,31 +15,35 @@ public class Monitor implements Listener {
     {
         rules = new HashMap<String, List<String>>();
         List<Map<?,?>> tmp = _config.getMapList("rules");
-        for (int i = 0; i < tmp.size(); ++i)
-        {
-            Map<String, List<String>> tmp1 = (Map) tmp.get(i);
-            for (Map.Entry<String, List<String>> j: tmp1.entrySet())
-            {
+        for (Map<?, ?> map : tmp) {
+            Map<String, List<String>> tmp1 = (Map) map;
+            for (Map.Entry<String, List<String>> j : tmp1.entrySet()) {
                 rules.put(j.getKey(), j.getValue());
-                Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>§e rule: " + j.getKey() + " ----> " + j.getValue().toString());
+                Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f> rule: " + j.getKey() + " ----> " + ChatColor.RESET + j.getValue().toString());
             }
         }
         Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>§e Found " + rules.size() + " rules.");
     }
     @EventHandler
     public void onPlayerAdvancementDoneEvent(PlayerAdvancementDoneEvent e) {
-
+        // get commands
         List<String> commands = rules.get(String.valueOf(e.getAdvancement().getKey()));
-        Bukkit.getConsoleSender().sendMessage(String.valueOf(e.getAdvancement().getKey()));
         if (commands == null) return;
-        Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>§e Player " + e.getPlayer().getDisplayName()
+        // vars
+        String uuid = e.getPlayer().getUniqueId().toString();
+        String player_name = e.getPlayer().getName();
+        String display_name = e.getPlayer().getDisplayName();
+        String advancement_id = e.getAdvancement().getKey().toString();
+        // log
+        Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>" + ChatColor.RESET + " Player " + e.getPlayer().getDisplayName()
                 + " has reached advancement " + e.getAdvancement().getKey() + ", and " + commands.size()
                 + " commands then execute.");
+        // execute
         for (String s:commands)
         {
-            Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>§e call: " + s);
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s);
+            String command = s.replaceAll("<adc:uuid>", uuid).replaceAll("<adc:player_name>", player_name).replaceAll("<adc:display_name>", display_name).replaceAll("<adc:advancement_id>", advancement_id);
+            Bukkit.getConsoleSender().sendMessage("§f<§aAdvancementCommand§f>" + ChatColor.RESET + " issued server command: " + command);
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command);
         }
-
     }
 }
